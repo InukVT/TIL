1add3b8d3f2dda6e35b378b5576dae669147c295
Bastian Inuk Christensen <inuk@bastians-macbook-air.local>
2018-06-19 10:37:31 +0200

    Init commit

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-
+        
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2")
     ],
