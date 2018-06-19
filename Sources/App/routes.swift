8c48d22b7e022b1002c0acf0d13fe668a5cd6afe
Bastian Inuk Christensen <inuk@bastians-macbook-air.local>
2018-06-19 10:52:00 +0200

    Removed typing error

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,7 +7,7 @@ public func routes(_ router: Router) throws {
         return try req.content.decode(Acronym.self)
             .flatMap(to: Acronym.self)
             { acronym in
-                return acronym.save(on:req)1
+                return acronym.save(on:req)
                 
         }
     }
