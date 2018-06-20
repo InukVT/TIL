15c45b3d99157e0a4a2f0bf58acd9956c8107d28
Bastian Inuk Christensen <jonalist@gmail.com>
2018-06-20 13:42:27 +0200

    Done some controlling and cleanup

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -12,16 +12,10 @@ struct AcronymController: RouteCollection {
             return Acronym.query(on: req).all()
         }
         
-        func createHandler(_ req: Request)
+        func createHandler(_ req: Request, acronym: Acronym)
         throws -> Future<Acronym>
         {
-            return try req.content
-                .decode(Acronym.self)
-                .flatMap(to: Acronym.self)
-                {
-                    acronym in
-                    return acronym.save(on: req)
-                }
+            return acronym.save(on: req)
         }
         
         func getHandler(_ req: Request)
@@ -34,17 +28,17 @@ struct AcronymController: RouteCollection {
         throws -> Future<Acronym>
         {
             return try flatMap(to: Acronym.self,
-            req.parameters.next(Acronym.self),
-            req.content.decode(Acronym.self))
-            {
-                acronym, updatedAcronym in
-                acronym.short = updatedAcronym.short
-                acronym.long = updatedAcronym.long
-                return acronym.save(on: req)
-            }
+                req.parameters.next(Acronym.self),
+                req.content.decode(Acronym.self))
+                {
+                    acronym, updatedAcronym in
+                    acronym.short = updatedAcronym.short
+                    acronym.long = updatedAcronym.long
+                    return acronym.save(on: req)
+                }
         }
         
-        func delteHandler(_ req: Request)
+        func deleteHandler(_ req: Request)
         throws -> Future<HTTPStatus>
         {
             return try req.parameters
@@ -68,6 +62,43 @@ struct AcronymController: RouteCollection {
             }.all()
         }
         
+        func getFirstHandler(_ req: Request)
+        throws -> Future<Acronym>
+        {
+            return Acronym.query(on: req)
+                .first()
+                .map(to: Acronym.self)
+                {   acronym in
+                    guard let acronym = acronym
+                        else
+                        {
+                            throw Abort(.notFound)
+                        }
+                    return acronym
+                }
+        }
+        
+        func sortedHandler(_ req: Request)
+        throws -> Future<[Acronym]>
+        {
+            return try Acronym.query(on: req)
+                .sort(\.short, .ascending).all()
+        }
+        
         acronymsRoutes.get(use: getAllHandler)
+        
+        acronymsRoutes.post(Acronym.self, use: createHandler)
+        
+        acronymsRoutes.get(Acronym.parameter, use: getHandler)
+        
+        acronymsRoutes.put(Acronym.parameter, use: updateHandler)
+        
+        acronymsRoutes.delete(Acronym.parameter, use: deleteHandler)
+        
+        acronymsRoutes.get("search", use: searchHandler)
+        
+        acronymsRoutes.get("first", use: getFirstHandler)
+        
+        acronymsRoutes.get("sorted", use: sortedHandler)
     }
 }
